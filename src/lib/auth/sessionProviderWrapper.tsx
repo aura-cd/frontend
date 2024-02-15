245961dee998d71c341b63c384fe58fea87ba67a
taku10101 <taku1011ht@gmail.com>
2024-02-15 17:45:16 +0900

    コンポーネントadd

---
+++ src/lib/auth/sessionProviderWrapper.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+
+const SessionProviderWrapper = ({ children }: { children: ReactNode }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default SessionProviderWrapper;
