ddf470769c0725a5441b219d3ce716f5c5e4b498
taku10101 <taku1011ht@gmail.com>
2024-02-16 11:11:42 +0900

    tab length type fix

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,15 +1,3 @@
-import React, { ReactNode } from "react";
-//配列の長さを合わせる
-type FixedLengthArray<
-  T extends string[] | ReactNode[],
-  N extends number
-> = N extends T["length"] ? T : never;
-
-type TabContentTuple<N extends number> = {
-  tabValue: FixedLengthArray<string[], N>;
-  content: FixedLengthArray<ReactNode[], N>;
-};
-
 const page = () => {
   return <div>fix</div>;
 };
--- src/components/TabComponent.tsx
+++ src/components/TabComponent.tsx
@@ -1,12 +1,17 @@
 import React, { ReactNode } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-interface TabComponentProps {
-  tabValue: string[];
-  content: ReactNode[];
-}
+type FixedLengthArray<
+  T extends string[] | ReactNode[],
+  N extends number
+> = N extends T["length"] ? T : never;
 
-const TabComponent = (props: TabComponentProps) => {
+type TabContentTuple<N extends number> = {
+  tabValue: FixedLengthArray<string[], N>;
+  content: FixedLengthArray<ReactNode[], N>;
+};
+
+const TabComponent = (props: TabContentTuple<2 | 3 | 4 | 5 | 6>) => {
   const { tabValue, content } = props;
   return (
     <Tabs defaultValue='account' className='w-[400px]'>
@@ -31,31 +36,3 @@ const TabComponent = (props: TabComponentProps) => {
 };
 
 export default TabComponent;
-
-// // const tabContent: TabContentTuple<6> = {
-// //   tabValue: [
-// //     "Account",
-// //     "Personal Information",
-// //     "Security",
-// //     "Notifications",
-// //     "Billing",
-// //     "Settings",
-// //   ],
-// //   content: [
-// //     <div>Hello</div>,
-// //     <div>World</div>,
-// //     "This is a string",
-// //     123,
-// //     [<div>Nested</div>, <div>Elements</div>],
-// //     <div>Extra Node</div>,
-// //   ],
-// // };
-
-// const page = () => {
-//   return (
-//     <>
-//       <TabComponent
-//         tabValue={tabContent.tabValue}
-//         content={tabContent.content}
-//       />
-//     </>
