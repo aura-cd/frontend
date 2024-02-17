88871cf7785b147500925a60ca3fe184632d61a2
taku10101 <taku1011ht@gmail.com>
2024-02-18 01:59:26 +0900

    page css add

---
+++ src/app/home/InstallAppButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import "@/styles/pageStyles/home.scss";
+
+const InstallAppButton = () => {
+  return (
+    <div>
+      <Button className='installAppButton'>InstallApp</Button>
+    </div>
+  );
+};
+
+export default InstallAppButton;
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,15 @@
 import Breadcrumb from "@/components/Breadcrumb";
 import DataTableComponent from "@/components/DetaTable/DataTableComponent";
-import React from "react";
-
+import "@/styles/pageStyles/home.scss";
+import ButtonArea from "./ButtonArea";
 const page = () => {
   return (
-    <div>
+    <div className='container'>
       <Breadcrumb />
-      <DataTableComponent />
+      <div className='mainContainer'>
+        <ButtonArea />
+        <DataTableComponent />
+      </div>
     </div>
   );
 };
