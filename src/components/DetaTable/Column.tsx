eac5f4f0674768e00544e59f4e476fa8dae0509f
taku10101 <taku1011ht@gmail.com>
2024-02-18 02:44:05 +0900

    component add

--- src/components/DetaTable/Column.tsx
+++ src/components/DetaTable/Column.tsx
@@ -42,6 +42,7 @@ export const columns: ColumnDef<Payment>[] = [
       <div className='capitalize'>{row.getValue("status")}</div>
     ),
   },
+
   {
     accessorKey: "email",
     header: ({ column }) => {
@@ -70,35 +71,6 @@ export const columns: ColumnDef<Payment>[] = [
       }).format(amount);
 
       return <div className='text-right font-medium'>{formatted}</div>;
-    },
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant='ghost' className='h-8 w-8 p-0'>
-              <span className='sr-only'>Open menu</span>
-              <DotsHorizontalIcon className='h-4 w-4' />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align='end'>
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
     },
   },
 ];
