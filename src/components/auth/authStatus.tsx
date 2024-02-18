eb9df5f7723c723bb5e319052ecbf93e8d4f6fe2
taku10101 <taku1011ht@gmail.com>
2024-02-18 23:57:45 +0900

    button shadow add

--- src/components/auth/authStatus.tsx
+++ src/components/auth/authStatus.tsx
@@ -2,6 +2,8 @@
 
 import { useSession, signIn, signOut } from "next-auth/react";
 import { useEffect } from "react";
+import { Button } from "../ui/button";
+import "@/styles/componentStyles/appButton.scss";
 
 async function keycloakSessionLogOut() {
   try {
@@ -13,7 +15,7 @@ async function keycloakSessionLogOut() {
 
 export default function AuthStatus() {
   const { data: session, status } = useSession();
-
+  console.log(session);
   useEffect(() => {
     if (
       status != "loading" &&
@@ -26,32 +28,27 @@ export default function AuthStatus() {
   }, [session, status]);
 
   if (status == "loading") {
-    return <div className='my-3'>Loading...</div>;
+    return <div className='mx-5'>Loading...</div>;
   } else if (session) {
     return (
-      <div className='my-3'>
-        <span className='text-yellow-100'>{session.user?.email}</span>{" "}
-        <button
-          className='bg-blue-900 font-bold text-white py-1 px-2 rounded border border-gray-50'
+      <div className='mx-5'>
+        <Button
+          className='installAppButton'
           onClick={() => {
             keycloakSessionLogOut().then(() => signOut({ callbackUrl: "/" }));
           }}
         >
           Log out
-        </button>
+        </Button>
       </div>
     );
   }
 
   return (
     <div className='my-3'>
-      Not logged in.{" "}
-      <button
-        className='bg-blue-900 font-bold text-white py-1 px-2 rounded border border-gray-50'
-        onClick={() => signIn("keycloak")}
-      >
+      <Button className='installAppButton' onClick={() => signIn("keycloak")}>
         Log in
-      </button>
+      </Button>
     </div>
   );
 }
