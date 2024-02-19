5d4cf5e3627ab83d61bc7f067a34d115fc77231c
taku10101 <taku1011ht@gmail.com>
2024-02-19 14:38:06 +0900

    feach add

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,8 +3,17 @@ import "@/styles//appButton.scss";
 import ButtonArea from "./components/ButtonArea";
 import AccordionComponent from "@/components/AccordionComponent";
 import { accordionData } from "./components/data";
+
 import Pankuzu from "@/components/path/Pankuzu";
+import useSWR from "swr";
 const page = () => {
+  try {
+    const { data } = useSWR("localhost:8080/home");
+    console;
+    console.log("data", data);
+  } catch (error) {
+    console.error("Error fetching data", error);
+  }
   return (
     <div className='container'>
       <Pankuzu />
