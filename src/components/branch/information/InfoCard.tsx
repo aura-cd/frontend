44744e3c735c5dd3fdaa0520a475fa01aceecfb6
taku10101 <taku1011ht@gmail.com>
2024-02-20 17:27:48 +0900

    card add

--- src/components/branch/information/InfoCard.tsx
+++ src/components/branch/information/InfoCard.tsx
@@ -5,11 +5,23 @@ import EditorArea from "./EditorArea";
 const InformationContents = () => {
   return (
     <div className='cardContainer'>
-      <div>
-        <div>card</div>
-        <div>card</div>
-        <div>card</div>
+      <div className='cardList'>
+        <div className='row-cardContainer'>
+          <h4>Image</h4>
+          <h3>slslslx</h3>
+        </div>
+
+        <div className='row-cardContainer'>
+          <h4>Health</h4>
+          <h3>slslslx</h3>
+        </div>
+
+        <div className='row-cardContainer'>
+          <h4>Age</h4>
+          <h3>slslslx</h3>
+        </div>
       </div>
+
       <div>
         <div>bar</div>
         <div>bar</div>
