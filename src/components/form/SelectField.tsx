56268df12fde06aa36da02c0b9870bea0b021c07
taku10101 <taku1011ht@gmail.com>
2024-02-21 03:47:41 +0900

    select form add

---
+++ src/components/form/SelectField.tsx
@@ -0,0 +1,47 @@
+import { Input } from "@/components/ui/input";
+import React from "react";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "../ui/form";
+import "@/styles/inputField.scss";
+interface InputFieldProps {
+  form: any;
+  name: string;
+  label: string;
+  placeholder: string;
+  option: string[];
+}
+
+const SelectField = (props: InputFieldProps) => {
+  const { name, label, form, option } = props;
+
+  return (
+    <div className='container'>
+      <FormField
+        control={form}
+        name={name}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{label}</FormLabel>
+            <FormControl>
+              <select {...field} className='select'>
+                {option.map((item, index) => (
+                  <option key={index} value={item}>
+                    {item}
+                  </option>
+                ))}
+              </select>
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+};
+
+export default SelectField;
