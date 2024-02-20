f4d364c72b09674871e3d3522766b71cce8742c6
taku10101 <taku1011ht@gmail.com>
2024-02-21 03:22:08 +0900

    ResourceForm fin

--- src/components/branch/option/from/GeneralForm.tsx
+++ src/components/branch/option/from/GeneralForm.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { generalSchema } from "./formSchema";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import InputField from "@/components/form/InputField";
+import "@/styles/loginForm.scss";
+// 3. Define your form schema.
 
-const GeneralForm = () => {
-  return <div>GeneralForm</div>;
-};
+function LoginForm() {
+  function onSubmit(values: z.infer<typeof generalSchema>) {
+    console.log(values);
+  }
 
-export default GeneralForm;
+  const form = useForm<z.infer<typeof generalSchema>>({
+    resolver: zodResolver(generalSchema),
+    defaultValues: {
+      pod_name: "",
+    },
+  });
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='form'>
+          <InputField
+            form={form.control}
+            name='pod_name'
+            label='pod_name'
+            placeholder='pod_name'
+          />
+
+          <Button className='button' type='submit'>
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
+
+export default LoginForm;
--- src/components/branch/option/from/formSchema.ts
+++ src/components/branch/option/from/formSchema.ts
@@ -13,14 +13,20 @@ export const resourceSchema = z.object({
   minReplicas: z.number().int().min(1).max(10),
   maxReplicas: z.number().int().min(1).max(10),
   targetPercentage: z.number().int().min(1).max(10),
+  container: z.string().transform((val) => Number(val)),
+  state: z.string().transform((val) => Number(val)),
+  resource: z.string().transform((val) => Number(val)),
+  target: z.string().transform((val) => Number(val)),
 });
 
 export const environmentSchema = z.object({
   kye: z.string().min(2).max(50),
   value: z.string().min(2).max(50),
+  config: z.boolean(),
 });
 
 export const ServiceSchema = z.object({
   kye: z.string().min(2).max(50),
   value: z.string().min(2).max(50),
+  config: z.boolean(),
 });
