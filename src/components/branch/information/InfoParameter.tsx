7aa5aef9051995be10acd5c7f98553e1e6deda31
taku10101 <taku1011ht@gmail.com>
2024-02-21 01:53:08 +0900

    icon add

--- src/components/branch/information/InfoParameter.tsx
+++ src/components/branch/information/InfoParameter.tsx
@@ -1,12 +1,18 @@
 import ProgressComponent from "@/components/ProgressComponent";
 import React from "react";
 import "./style/InfoParameter.scss";
+import { HiMiniCpuChip } from "react-icons/hi2";
+import { FaMemory } from "react-icons/fa";
+import { GrStorage } from "react-icons/gr";
+
 const InfoParameter = () => {
   return (
     <div className='parameter-container'>
       <div className='parameter-row'>
         <div className='head'>
-          <h4>icon </h4>
+          <div className='icons'>
+            <HiMiniCpuChip />
+          </div>
           <h4>CPU usage(Ave.)</h4>
         </div>
         <div className='data-container'>
@@ -16,8 +22,10 @@ const InfoParameter = () => {
       </div>
       <div className='parameter-row'>
         <div className='head'>
-          <h4>icon </h4>
-          <h4>CPU usage(Ave.)</h4>
+          <div className='icons'>
+            <FaMemory />
+          </div>
+          <h4>Mem usage(Ave.)</h4>
         </div>
         <div className='data-container'>
           <h6>50% (150m/300m)</h6>
@@ -26,7 +34,9 @@ const InfoParameter = () => {
       </div>
       <div className='parameter-row'>
         <div className='head'>
-          <h4>icon </h4>
+          <div className='icons'>
+            <GrStorage />
+          </div>
           <h4>CPU usage(Ave.)</h4>
         </div>
         <div className='data-container'>
