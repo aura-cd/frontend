1f88956f86f5ad4bb9d17791ffedb432f1199ffc
xpadev <xpadev@gmail.com>
2024-02-24 04:26:54 +0900

    fix: move options

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getIdToken } from "@/components/auth/sessionTokenAccessor";
+import { authOptions } from "@/app/api/auth/options";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
