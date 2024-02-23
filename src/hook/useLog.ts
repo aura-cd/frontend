2e138c5402979a832273a3d207d5a9f33844a0b4
xpadev <xpadev@gmail.com>
2024-02-23 16:15:54 +0900

    log-viewer-hook/fix: useEffect dependencies

--- src/hook/useLog.ts
+++ src/hook/useLog.ts
@@ -5,14 +5,15 @@ const useLog = (organization: string, pod: string) => {
   const sseRef = useRef<EventSource>();
   const [log, setLog] = useState<string[]>([]);
   useEffect(() => {
-    sseRef.current = new EventSource(`http://localhost:8888/organizations/${organization}/pods/${pod}/log`);
+    setLog([]);
+    sseRef.current = new EventSource(`${process.env.NEXT_PUBLIC_API_URL}organizations/${organization}/pods/${pod}/log`);
     sseRef.current.onmessage = (event) => {
       setLog((prevLog) => [...prevLog, event.data]);
     };
     return () => {
       sseRef.current?.close();
     };
-  }, []);
+  }, [organization,pod]);
   return log;
 }
 
